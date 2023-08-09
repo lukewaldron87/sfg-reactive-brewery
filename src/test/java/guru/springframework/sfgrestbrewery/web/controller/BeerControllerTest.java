0f42c71b47dc720758c2ba4530c827a8016d8c84
Luke Waldron <lukewaldron87@gmail.com>
2023-08-09 11:15:59 +0200

    assignment

--- src/test/java/guru/springframework/sfgrestbrewery/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/sfgrestbrewery/web/controller/BeerControllerTest.java
@@ -1,22 +1,29 @@
 package guru.springframework.sfgrestbrewery.web.controller;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 import guru.springframework.sfgrestbrewery.bootstrap.BeerLoader;
 import guru.springframework.sfgrestbrewery.services.BeerService;
 import guru.springframework.sfgrestbrewery.web.model.BeerDto;
+import guru.springframework.sfgrestbrewery.web.model.BeerPagedList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 @WebFluxTest(BeerController.class)
 class BeerControllerTest {
@@ -39,8 +46,23 @@ class BeerControllerTest {
     }
 
     @Test
-    void getBeerById() {
+    void listBeers() {
+        BeerPagedList beerPagedList = new BeerPagedList(Arrays.asList(validBeer, validBeer));
 
+        given(beerService.listBeers(any(), any(), any(), any())).willReturn(beerPagedList);
+
+        webTestClient.get()
+                .uri("/api/v1/beer")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(BeerPagedList.class)
+                .value(returnedList -> returnedList.getSize(), equalTo(beerPagedList.getSize()));
+
+    }
+
+    @Test
+    void getBeerById() {
         UUID beerId = UUID.randomUUID();
         given(beerService.getById(any(), any())).willReturn(validBeer);
 
@@ -51,5 +73,21 @@ class BeerControllerTest {
                 .expectStatus().isOk()
                 .expectBody(BeerDto.class)
                 .value(beerDto -> beerDto.getBeerName(), equalTo(validBeer.getBeerName()));
+    }
+
+    @Test
+    void getBeerByUpc() {
+        String upc = "123456789";
+
+        given(beerService.getByUpc(upc)).willReturn(validBeer);
+
+        webTestClient.get()
+                .uri("/api/v1/beerUpc/" + upc)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(BeerDto.class)
+                .value(beerDto -> beerDto.getBeerName(), equalTo(validBeer.getBeerName()));
+
     }
 }
